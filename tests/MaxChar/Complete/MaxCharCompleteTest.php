af6d57e9f3e192addda248bee36b6f465926b673
ahmed <ahmed_aboud_ali@yahoo.com>
2019-04-27 19:29:21 +0200

    Added Test Case for get1 function for MaxChar

--- tests/MaxChar/Complete/MaxCharCompleteTest.php
+++ tests/MaxChar/Complete/MaxCharCompleteTest.php
@@ -32,4 +32,9 @@ final class MaxCharCompleteTest extends TestCase
     {
         self::assertSame('2', MaxCharComplete::get('apple 2202'));
     }
+    
+    public function testGet1MaxCharWithNumbers(): void
+    {
+        self::assertSame('2', MaxCharComplete::get1('apple 2202'));
+    }
 }
