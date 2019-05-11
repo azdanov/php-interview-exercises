e60e7fe74fd0b680f474e1ed33f0b91d2b0eb8cb
Anton Zdanov <azdanov@pm.me>
2019-05-11 08:33:21 +0300

    style(Reverse): add use statements

--- exercises/Reverse/Complete/ReverseComplete.php
+++ exercises/Reverse/Complete/ReverseComplete.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace Exercises\Reverse\Complete;
 
+use function abs;
 use function array_reduce;
 use function array_reverse;
 use function implode;
+use function log10;
+use function mb_strlen;
+use function mb_substr;
+use function pow;
 use function str_split;
 use function strlen;
 use function strrev;
