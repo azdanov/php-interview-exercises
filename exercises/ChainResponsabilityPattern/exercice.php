61df83827a7fbdaa7acb10fe664e84b5c1ace262
kevin <kevin.schmitt.upjv@gmail.com>
2019-10-04 10:02:02 +0200

    update: exaplanation of pattern and exercice

--- exercises/ChainResponsabilityPattern/exercice.php
+++ exercises/ChainResponsabilityPattern/exercice.php
@@ -1,34 +1,26 @@
 <?php
  
-namespace Exercises\ChainResponsabilityPattern;
-require __DIR__.'/../../vendor/autoload.php';
+
 /**
- * Info Chain of responsibility pattern design permit to classes try response to requests without know capacity of other objects.
- * Exercice we need of filter that permit to :
- *      - if data pass like argument is string then this data return uppercase of this data
- *      - if data pass like argument is int then this data return multiple by 2
+ * Info Chain of responsibility pattern design permit objects to try response to requests without know capacity of other objects to respond.
+ * Exercice we need filters that permit to :
+ *      - if data pass in filter is string then this data return like uppercase 
+ *      - if data pass in filter is (int or string contain int) then return multiple of this data 
+ * Help: use Abstract Method permit to filter if you have to use successor if exist. Use abstract method doFilter for real implementation of filter (multiple and uppercase)
+ * Try before read solution ;)
  * Example:
+ *          $upperCaseFilter = new UpperCaseFilter();
+ *          $multipleFilter = new MultipleFilter();
+ *          $multipleFilter->setMultiple(3);
+ *          $uppercaseFilter->setSuccessor($uppercaseFilter);
+ * 
  *          echo $mainFilter.filter('kevin schmitt'); KEVIN SCHMITT
  *          echo $mainFilter.filter('360'); 24
  *          echo $mainFilter.filter(12); 24
 */
 
-use  Exercises\ChainResponsabilityPattern\Complete\MultipleFilter;
-use  Exercises\ChainResponsabilityPattern\Complete\UpperCaseFilter;
 
-/**
- *  Read after try !!!
- *  Help use abstract method MainFilter and create filter for each filter DoubleFilter  UpperCaseFilter 
- */
 
-$upperCaseFilter = new UpperCaseFilter();
-$myFilter = new MultipleFilter();
-$myFilter->setSuccessor($upperCaseFilter)
-         ->setMultiple(5);
 
-$space = ' ';
 
-echo $myFilter->filter('hello') . $space;
-echo $myFilter->filter('24') . $space;
-echo $myFilter->filter(360) . $space;
-echo $myFilter->filter(0) . $space;
+
