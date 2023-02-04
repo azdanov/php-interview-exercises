0803c0319eb8908878c5f279084a857a91f6fad4
Eduardo <aruku64@gmail.com>
2023-02-04 17:10:49 +0100

    Remove wrong implementation of MaxSubArraySum

--- tests/MaxSubArraySum/Complete/MaxSubArrayCompleteTest.php
+++ tests/MaxSubArraySum/Complete/MaxSubArrayCompleteTest.php
@@ -16,10 +16,6 @@ final class MaxSubArrayCompleteTest extends TestCase
             method_exists(MaxSubArraySumComplete::class, 'max1'),
             'Class does not have static method max1'
         );
-        self::assertTrue(
-            method_exists(MaxSubArraySumComplete::class, 'max2'),
-            'Class does not have static method max2'
-        );
     }
 
     public function testMax1(): void
@@ -28,16 +24,6 @@ final class MaxSubArrayCompleteTest extends TestCase
         self::assertSame(
             19,
             MaxSubArraySumComplete::max1([2, 6, 9, 2, 1, 8, 5, 6, 3], 3)
-        );
-        self::assertSame(0, MaxSubArraySumComplete::max1([1, 2, 3], 5));
-    }
-
-    public function testMax2(): void
-    {
-        self::assertSame(5, MaxSubArraySumComplete::max2([1, 2, 3], 2));
-        self::assertSame(
-            19,
-            MaxSubArraySumComplete::max2([2, 6, 9, 2, 1, 8, 5, 6, 3], 3)
         );
         self::assertSame(0, MaxSubArraySumComplete::max1([1, 2, 3], 5));
     }
